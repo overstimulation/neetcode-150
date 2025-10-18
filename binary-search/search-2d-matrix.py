ad687c41c7019587a295e6f2a267ddcc07b868f3
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-10-18 13:36:25 +0200

    search-2d-matrix

---
+++ binary-search/search-2d-matrix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n
+            mid_value = matrix[row][col]
+
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
