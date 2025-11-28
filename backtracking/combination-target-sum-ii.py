a0d32e63f7d20191ba34694c04295f91febec7fd
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-11-28 16:50:32 +0100

    combination-target-sum-ii

---
+++ backtracking/combination-target-sum-ii.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def backtrack(pos, curr, target):
+            if target == 0:
+                res.append(curr.copy())
+                return
+            if target <= 0:
+                return
+
+            prev = -1
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev:
+                    continue
+
+                curr.append(candidates[i])
+                backtrack(i + 1, curr, target - candidates[i])
+                curr.pop()
+                prev = candidates[i]
+
+        backtrack(0, [], target)
+        return res
