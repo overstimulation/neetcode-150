13bacd30a16a6829b500be02bbc8dc49621ca877
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-01-24 12:34:28 +0100

    merge-triplets-to-form-target

---
+++ greedy/merge-triplets-to-form-target.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeTriplets(self, triplets: list[list[int]], target: list[int]) -> bool:
+        good = set()
+
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            for i in range(3):
+                if t[i] == target[i]:
+                    good.add(i)
+
+        return len(good) == 3
