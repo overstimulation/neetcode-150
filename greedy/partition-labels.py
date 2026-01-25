e0b7989d396179422ffea682d136aa4c38c883dc
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-01-25 15:47:58 +0100

    partition-labels

---
+++ greedy/partition-labels.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        last_index = {c: i for i, c in enumerate(s)}
+
+        result = []
+        start = 0
+        end = 0
+
+        for i, c in enumerate(s):
+            end = max(end, last_index[c])
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+
+        return result
