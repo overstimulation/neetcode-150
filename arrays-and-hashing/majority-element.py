9330587e3d200986e0b430ec5570c45c73383ad1
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-02-20 08:25:43 +0100

    majority-element

---
+++ arrays-and-hashing/majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        count = 0
+        candidate = 0
+        for n in nums:
+            if count == 0:
+                candidate = n
+            count += 1 if n == candidate else -1
+        return candidate
