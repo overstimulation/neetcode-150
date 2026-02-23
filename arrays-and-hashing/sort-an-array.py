497a831f0aaa113166ef659a7f63bd996d861cc3
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-02-23 14:16:20 +0100

    sort-an-array

---
+++ arrays-and-hashing/sort-an-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+
+        res = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+
+        while i < len(left):
+            res.append(left[i])
+            i += 1
+        while j < len(right):
+            res.append(right[j])
+            j += 1
+
+        return res
