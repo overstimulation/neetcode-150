09653271529ab3320438d59be6beb1b44a034668
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-03-31 09:02:01 +0200

    binary-tree-inorder-traversal

---
+++ trees/binary-tree-inorder-traversal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def inorderTraversal(self, root: TreeNode | None) -> list[int]:
+        res = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
