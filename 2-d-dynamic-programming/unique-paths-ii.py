e8fd0bab35dda3e68de535d0f2041c4f40961c5b
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-05-14 10:01:45 +0200

    unique-paths-ii

---
+++ 2-d-dynamic-programming/unique-paths-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        if not obstacleGrid or not obstacleGrid[0] or obstacleGrid[0][0] == 1:
+            return 0
+
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+        dp[0] = 1
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+
+        return dp[-1]
